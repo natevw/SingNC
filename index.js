5e67c2655082054bbdc23340b8daf3c4fb746970
Nathan Vander Wilt <nate@calftrail.com>
2015-03-24 22:29:48 -0700

    start testing on device

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var midi = require('midi');
+var midi = require('midi'),
+    com1 = require('serialport');
 
+
+var axes = ['X', 'Y', 'Z', 'A', 'B', 'C'],
+    mult = [-0.005, 0.005, -0.0001];
+axes.length = 3;      // truncate to what machine has
 
 function freq(n) {      // using http://newt.phys.unsw.edu.au/jw/notes.html temperment
   return Math.pow(2, (n-69) / 12) * 440;
 }
 
-function vector(freqs, time) {
-  freqs.sort().reverse();
-  
-}
+var state = {
+  notes: [],
+  mtime: 100
+};
+setInterval(function () {
+  var mvmt = state.notes.map(freq).sort().reverse().slice(0, axes.length).map(function (f, i) {
+    return axes[i]+(f*mult[i]).toFixed(4);
+  });
+  if (mvmt.length) {
+    var cmd = ['F'+(1/60e3/state.mtime).toFixed(12)].concat(mvmt).join(' ');
+    sendCommand(cmd);
+  }
+}, state.mtime);
 
-
-var poll = setInterval(function () {
+var poll_midi = setInterval(function () {
   var numInputs = input.getPortCount();
   if (!numInputs) return;
-  else clearInterval(poll);
+  else clearInterval(poll_midi);
   
   console.log("Input connected:", input.getPortName(0));
   input.openPort(0);
@@ -29,7 +42,43 @@ var poll = setInterval(function () {
     } else if (cmd === 0x90) {
       notes[msg[1]]= msg[2];
     }
-    console.log("Notes:", Object.keys(notes).map(freq));
+    state.notes = Object.keys(notes);
   });
   //input.closePort();
 }, 1e3), input = new midi.input();
+
+var cncPort;
+function poll_com1() {
+  com1.list(function (e, arr) {
+    if (e) throw e;
+    var cnc = arr.filter(function (port) {
+      return port.locationId;
+    })[0];
+    if (!cnc) setTimeout(poll_com1, 1e3);
+    else new com1.SerialPort(cnc.comName, {
+      baudrate: 115200,
+      parser: com1.parsers.readline("\n")
+    }).on('open', function () {
+      console.log("Output opened:", cnc.comName);
+      cncPort = this;
+      cncPort.once('data', function () {
+        // unlock, initialize necessary motion modes
+        //sendCommand("$X");
+        sendCommand("$H");
+        sendCommand("G0 X0 Y-190");
+        sendCommand("G1 G21 G91 G93 F42");
+      });
+      cncPort.on('data', function (d) {
+        console.log(d.toString());
+      });
+    }).on('error', function (e) {
+      console.error(e);
+      cncPort = null;
+    });
+  });
+}
+function sendCommand(str) {
+  console.log(str, (cncPort) ? '' : '~');
+  if (cncPort) cncPort.write(str+'\n');
+}
+poll_com1();
